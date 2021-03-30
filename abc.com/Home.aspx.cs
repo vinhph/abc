8c5fc5ece7df09bb6d3c1f9f64e4307094006396
vinhph <42417393+vinhph@users.noreply.github.com>
2021-03-30 15:43:36 +0700

    Update Home.aspx.cs

--- abc.com/Home.aspx.cs
+++ abc.com/Home.aspx.cs
@@ -11,7 +11,7 @@ namespace abc.com
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            ltrMessage.Text = "This is the four version";
+            ltrMessage.Text = "This is the five version";
         }
     }
 }
