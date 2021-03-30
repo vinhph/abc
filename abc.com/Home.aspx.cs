f3f5174d7b6fb37d037329f9ce928fc9377deb0e
vinhph <42417393+vinhph@users.noreply.github.com>
2021-03-30 22:39:10 +0700

    Update Home.aspx.cs

--- abc.com/Home.aspx.cs
+++ abc.com/Home.aspx.cs
@@ -11,7 +11,7 @@ namespace abc.com
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            ltrMessage.Text = "This is the 11 version";
+            ltrMessage.Text = "This is the 12 version";
         }
     }
 }
